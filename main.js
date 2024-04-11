b88fb309b08730d8bc6289507c4d1aaf0bb9e191
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-04-11 23:29:48 +0100

    bonsai

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-function myFunction(element) {
-    var content = element.nextElementSibling;
-  
-    // Close all other sections before opening the clicked one
-    var allSections = document.querySelectorAll('.accordion-content');
-    allSections.forEach(function (section) {
-      if (section !== content && section.classList.contains('show')) {
-        section.classList.remove('show');
-      }
-    });
-  
-    // Toggle the 'show' class to display or hide the content
-    content.classList.toggle('show');
+// eslint-disable-next-line no-unused-vars
+function myFunction () {
+  const x = document.getElementById('myLinks')
+  if (x.style.display === 'none') {
+    x.style.display = 'block'
+  } else {
+    x.style.display = 'none'
   }
+}
