253f686860b26d7d0dc26721ccdb6753a57bdd69
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-04-23 12:44:01 +0100

    main

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-// eslint-disable-next-line no-unused-vars
-function myFunction () {
-  const x = document.getElementById('myLinks')
-  if (x.style.display === 'none') {
-    x.style.display = 'block'
-  } else {
-    x.style.display = 'none'
-  }
+
+const accordion = document.getElementsByClassName('faqContainer')
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
 }
