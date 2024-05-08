dff33434a218fa6cfea52ac6095434187f8f6475
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-05-08 23:55:22 +0100

    switch button

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-// switch button
-document.addEventListener('DOMContentLoaded', function () {
-  const switchbtn = document.getElementById('switchbutton')
-  const yc = document.getElementById('yearContainer')
-  switchbtn.addEventListener('click', function () {
-    switchbtn.style.transform = 'translateX(40px)'
-    if (yc.style.display === 'none') {
-      yc.style.display = 'block'
-    } else {
-      yc.style.display = 'none'
-    }
+// eslint-disable-next-line no-unused-vars
+const switchButton = document.getElementById('switchbtn').addEventListener('click', slider)
+
+function slider () {
+  const year = document.getElementById('yearContainer')
+
+  if (year.style.display === 'none') {
+    year.style.display = 'block'
+  } else {
+    year.style.display = 'none'
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const changeButton = document.getElementById('switchbtn').addEventListener('click', changebtn)
+
+function changebtn () {
+  const month = document.getElementById('monthlyContainer')
+
+  if (month.style.display === 'block') {
+    month.style.display = 'none'
+  } else {
+    month.style.display = 'block'
+  }
+}
+
+const accordion = document.getElementsByClassName('faqContainer')
+
+// eslint-disable-next-line no-undef
+for (i = 0; i < accordion.length; i++) {
+  // eslint-disable-next-line no-undef
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
   })
-})
+}
